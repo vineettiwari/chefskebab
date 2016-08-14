ea02accf948ea160836f1ade7f6375c391a8db4e
vineetTiwari <vineet01@hotmail.com>
2016-08-13 21:14:40 -0700

    Initial commit for chef's kebab.

---
+++ assets/js/menu.js
@@ -0,0 +1,46 @@
+
+(function($) {
+
+// Menu Animated Modals //
+
+  // Row 1 //
+	$("#drinksLink").animatedModal({
+		modalTarget: "drinksModal"
+	});
+	$("#appetizerLink").animatedModal({
+		modalTarget: "appetizerModal"
+	});
+	$("#chaatLink").animatedModal({
+		modalTarget: "chaatModal"
+	});
+	$("#kebabsLink").animatedModal({
+		modalTarget: "kebabsModal"
+	});
+  // Row 2 //
+  $("#vegeLink").animatedModal({
+		modalTarget: "vegeModal"
+	});
+	$("#chickenLink").animatedModal({
+		modalTarget: "chickenModal"
+	});
+	$("#meatLink").animatedModal({
+		modalTarget: "meatModal"
+	});
+	$("#seafoodLink").animatedModal({
+		modalTarget: "seafoodModal"
+	});
+  // Row 3 //
+  $("#riceLink").animatedModal({
+		modalTarget: "riceModal"
+	});
+	$("#tandooriLink").animatedModal({
+		modalTarget: "tandooriModal"
+	});
+	$("#sideLink").animatedModal({
+		modalTarget: "sideModal"
+	});
+	$("#dessertLink").animatedModal({
+		modalTarget: "dessertModal"
+	});
+	
+})(jQuery);
